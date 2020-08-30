c7a63238376f4d6bbba1ff2915422de72ab8de62
fanjihao <1181190638@qq.com>
2020-08-30 12:00:04 +0800

    Updates

---
+++ precache-manifest.fe9a3795bb1fd79b1dc12d76fccca50c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3356072beefdc35c811781cf2b4aa2f8",
+    "url": "/react-wsjj/index.html"
+  },
+  {
+    "revision": "b0a796f14819cb617832",
+    "url": "/react-wsjj/static/css/2.04ff9426.chunk.css"
+  },
+  {
+    "revision": "0b08a96326e9be6a757d",
+    "url": "/react-wsjj/static/css/main.68cf8426.chunk.css"
+  },
+  {
+    "revision": "b0a796f14819cb617832",
+    "url": "/react-wsjj/static/js/2.f4b6eb0a.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/react-wsjj/static/js/2.f4b6eb0a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b08a96326e9be6a757d",
+    "url": "/react-wsjj/static/js/main.53d007ce.chunk.js"
+  },
+  {
+    "revision": "bbafbd5247340177d217",
+    "url": "/react-wsjj/static/js/runtime-main.b67c76d0.js"
+  }
+]);
